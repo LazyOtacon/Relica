b3ce3667ee6474e74ae687e2f7f554b4f093176b
otacon <Luvtogame01@gmail.com>
2019-12-09 14:27:18 -0500

    Fire arrows
    fire arrows

---
+++ Relica/Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        // rBody.velocity = new Vector2(10, 0);
+        rBody.velocity = transform.right * 10;
+    }
+}
---
+++ Relica/Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform bow;
+    public GameObject arrow;
+    public float arrowSpeed = 10f;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fire();
+        }
+
+
+    }
+
+
+    void fire()
+    {
+        GameObject Arrow = Instantiate(arrow, bow.position, bow.rotation);
+        Rigidbody2D rBody = arrow.GetComponent<Rigidbody2D>();
+        rBody.AddForce(bow.up * arrowSpeed, ForceMode2D.Impulse);
+    }
+
+}
