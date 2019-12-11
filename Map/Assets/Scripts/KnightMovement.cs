1f9894ebfd980e0afc2e32f9d147010a64e477c5
Eric Frey <eric.frey@rogers.com>
2019-12-10 19:06:31 -0500

    Nearly done version
    (I had many commits already but Joseph accidentally deleted them when he force pushed his stuff, because he was unsure of how to do it)

---
+++ Map/Assets/Scripts/KnightMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : MonoBehaviour
+{
+    public float speed = 20.0f;
+    int counter = 0;
+
+    private Rigidbody2D rBody;
+    private void FixedUpdate()
+    {
+        if (counter >= 0 && counter < 20)
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            rBody.velocity = Vector2.right * speed;
+            counter++;
+        }
+        if (counter == 20)
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            rBody.velocity = Vector2.right * speed;
+            counter = 39;
+        }
+        if (counter == 21)
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            rBody.velocity = Vector2.left * speed;
+            counter = 0;
+        }
+        if (counter <= 39 && counter > 21)
+        {
+            rBody = GetComponent<Rigidbody2D>();
+            rBody.velocity = Vector2.left * speed;
+            counter--;
+        }
+    }
+}
