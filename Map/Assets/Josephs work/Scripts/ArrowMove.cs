7a9b972c72e9eed802b8dae913cbfec43da41b26
Eric Frey <eric.frey@rogers.com>
2019-12-11 12:56:52 -0500

    Actually it wasn't done but it is close
    I lied there is more to do but it is almost finished

--- Map/Assets/Josephs work/Scripts/ArrowMove.cs
+++ Map/Assets/Josephs work/Scripts/ArrowMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ArrowMove : MonoBehaviour
 {
     private Rigidbody2D rBody;
-    public GameObject player;
+    private GameObject player;
     public GameObject arrow;
 
     // Start is called before the first frame update
@@ -13,7 +13,8 @@ public class ArrowMove : MonoBehaviour
     {
         rBody = GetComponent<Rigidbody2D>();
         rBody.velocity = transform.right * 10;
-      //  transform.rotation = Quaternion.LookRotation(rBody.velocity);
+        //  transform.rotation = Quaternion.LookRotation(rBody.velocity);
+        player = GameObject.FindWithTag("Player");
     }
 
 
@@ -22,7 +23,8 @@ public class ArrowMove : MonoBehaviour
     {
         if (collision.gameObject)
         {
-            //  player.gameObject.transform.position =  new Vector2(arrow.gameObject.transform.position.x, arrow.gameObject.transform.position.y);
+            // player.gameObject.transform.position =  new Vector2(arrow.gameObject.transform.position.x, arrow.gameObject.transform.position.y);
+            player.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y + 1, collision.gameObject.transform.position.z);
 
             Destroy(gameObject);
         }
